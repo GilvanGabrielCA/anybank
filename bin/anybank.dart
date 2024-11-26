728894a1d29feec99b81616a1c822c013128fd78
Gilvan Gabriel <93411051+GilvanGabrielCA@users.noreply.github.com>
2024-11-26 14:31:52 -0400

    Herança e Polimorfismo aplicados no projeto anybank

--- bin/anybank.dart
+++ bin/anybank.dart
@@ -3,6 +3,8 @@ import 'package:anybank/conta.dart';
 void main() {
   Conta contaMatheus = Conta("Matheus", 1000);
   Conta contaRoberta = Conta("Roberta", 2000);
+  ContaCorrente contaChris = ContaCorrente("Chris", 4000);
+  ContaPoupanca contaDenise = ContaPoupanca("Denise", 4000);
 
   List<Conta> contas = <Conta>[contaMatheus, contaRoberta];
 
@@ -13,5 +15,13 @@ void main() {
   contaRoberta.receber(3000);
   contaMatheus.receber(500);
   contaMatheus.enviar(200);
+
+  contaChris.imprimeSaldo();
+  contaChris.enviar(4300);
+
+  contaDenise.imprimeSaldo();
+  contaDenise.enviar(4300);
+  contaDenise.calculaRendimento();
+  contaDenise.imprimeSaldo();
 }
 
