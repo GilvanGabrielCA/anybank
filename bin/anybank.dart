1ee0e750266e5e0d05c1363739d12466e195f361
Gilvan Gabriel <93411051+GilvanGabrielCA@users.noreply.github.com>
2024-11-26 13:35:16 -0400

    Metodos novos criados e arquivo externo para classe conta

--- bin/anybank.dart
+++ bin/anybank.dart
@@ -1,18 +1,17 @@
-void main(){
+import 'package:anybank/conta.dart';
+
+void main() {
   Conta contaMatheus = Conta("Matheus", 1000);
   Conta contaRoberta = Conta("Roberta", 2000);
 
   List<Conta> contas = <Conta>[contaMatheus, contaRoberta];
 
- for (Conta conta in contas){
-  print(conta.titular);
-  print(conta.saldo);
- }
+  for (Conta conta in contas) {
+    conta.imprimeSaldo();
+  }
+  
+  contaRoberta.receber(3000);
+  contaMatheus.receber(500);
+  contaMatheus.enviar(200);
 }
 
-class Conta{
-  String titular;
-  double saldo;
-
-  Conta(this.titular, this.saldo); //construtor
-}
---
+++ lib/conta.dart
@@ -0,0 +1,23 @@
+class Conta {
+  String titular;
+  double _saldo;
+
+  Conta(this.titular, this._saldo); //construtor
+
+  // métodos
+  void receber(double valor) {
+    _saldo += valor;
+    imprimeSaldo();
+  }
+
+  void enviar(double valor) {
+    if (_saldo >= valor) {
+      _saldo -= valor;
+      imprimeSaldo();
+    }    
+  }
+
+  void imprimeSaldo(){
+    print("O saldo atual de $titular, é: R\$$_saldo");
+  }
+}
